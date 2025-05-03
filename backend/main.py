2e32b547de96b6b6bb6b271e43b487397e2bd5f7
rahullkiran <rahulkiran698@gmail.com>
2025-05-03 10:47:36 -0400

    small changes
    removed few lines

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from monitor import check_stats, update_thresholds, get_thresholds, get_history
 
-
 app = FastAPI()
 
 # Allow frontend to talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to your frontend origin
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
